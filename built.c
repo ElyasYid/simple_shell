30880f1771d3b14f9aaf3bbf74ca79be1ee77344
Elias Yidnekachew <140306064+ElyasYid@users.noreply.github.com>
2023-11-15 14:21:43 +0300

    Update built.c

--- built.c
+++ built.c
@@ -2,19 +2,17 @@
 
 /**
  * _myexit - exits the shell
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * @info: Structure containing potential arguments
  *  Return: exits with a given exit status
- *         (0) if info.argv[0] != "exit"
  */
 int _myexit(info_t *info)
 {
-	int exitcheck;
+	int chexit;
 
-	if (info->argv[1])  /* If there is an exit arguement */
+	if (info->argv[1])
 	{
-		exitcheck = _erratoi(info->argv[1]);
-		if (exitcheck == -1)
+		chexit = _erratoi(info->argv[1]);
+		if (chexit == -1)
 		{
 			info->status = 2;
 			print_error(info, "Illegal number: ");
@@ -28,45 +26,41 @@ int _myexit(info_t *info)
 	info->err_num = -1;
 	return (-2);
 }
-
 /**
  * _mycd - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
+ * @info: Structure containing potential arguments
+ * Return: Always 0
  */
 int _mycd(info_t *info)
 {
-	char *s, *dir, buffer[1024];
-	int chdir_ret;
+	char *bobo, *coco, buffer[1024];
+	int roro;
 
-	s = getcwd(buffer, 1024);
-	if (!s)
-		_puts("TODO: >>getcwd failure emsg here<<\n");
+	bobo = getcwd(buffer, 1024);
+	if (!bobo)
+		_puts("getcwdfail\n");
 	if (!info->argv[1])
 	{
-		dir = _getenv(info, "HOME=");
-		if (!dir)
-			chdir_ret = /* TODO: what should this be? */
-				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		coco = _getenv(info, "HOME=");
+		if (!coco)
+			chdir((coco = _getenv(info, "PWD=")) ? coco : "/");
 		else
-			chdir_ret = chdir(dir);
+			roro = chdir(coco);
 	}
 	else if (_strcmp(info->argv[1], "-") == 0)
 	{
 		if (!_getenv(info, "OLDPWD="))
 		{
-			_puts(s);
+			_puts(bobo);
 			_putchar('\n');
 			return (1);
 		}
 		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-		chdir_ret = /* TODO: what should this be? */
-			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+		chdir((coco = _getenv(info, "OLDPWD=")) ? coco : "/");
 	}
 	else
-		chdir_ret = chdir(info->argv[1]);
-	if (chdir_ret == -1)
+		roro = chdir(info->argv[1]);
+	if (roro == -1)
 	{
 		print_error(info, "can't cd to ");
 		_eputs(info->argv[1]), _eputchar('\n');
