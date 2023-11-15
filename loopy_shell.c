bc6ced9282d467845ff2be62dc978d2d7fde4d45
Elias Yidnekachew <140306064+ElyasYid@users.noreply.github.com>
2023-11-15 15:01:06 +0300

    Update loopy_shell.c

--- loopy_shell.c
+++ loopy_shell.c
@@ -3,8 +3,7 @@
 /**
  * hsh - main shell loop
  * @info: the parameter & return info struct
- * @av: the argument vector from main()
- *
+ * @av: the argument vector from main
  * Return: 0 on success, 1 on error, or error code
  */
 int hsh(info_t *info, char **av)
@@ -46,11 +45,7 @@ int hsh(info_t *info, char **av)
 /**
  * find_builtin - finds a builtin command
  * @info: the parameter & return info struct
- *
- * Return: -1 if builtin not found,
- *			0 if builtin executed successfully,
- *			1 if builtin found but not successful,
- *			-2 if builtin signals exit()
+ * Return: 0 if succes
  */
 int find_builtin(info_t *info)
 {
@@ -80,7 +75,6 @@ int find_builtin(info_t *info)
 /**
  * find_cmd - finds a command in PATH
  * @info: the parameter & return info struct
- *
  * Return: void
  */
 void find_cmd(info_t *info)
@@ -122,7 +116,6 @@ void find_cmd(info_t *info)
 /**
  * fork_cmd - forks a an exec thread to run cmd
  * @info: the parameter & return info struct
- *
  * Return: void
  */
 void fork_cmd(info_t *info)
@@ -132,7 +125,6 @@ void fork_cmd(info_t *info)
 	child_pid = fork();
 	if (child_pid == -1)
 	{
-		/* TODO: PUT ERROR FUNCTION */
 		perror("Error:");
 		return;
 	}
@@ -145,7 +137,6 @@ void fork_cmd(info_t *info)
 				exit(126);
 			exit(1);
 		}
-		/* TODO: PUT ERROR FUNCTION */
 	}
 	else
 	{
