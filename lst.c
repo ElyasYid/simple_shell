86cdf1d0f466d11a67822b28fdc666d20037d539
Elias Yidnekachew <140306064+ElyasYid@users.noreply.github.com>
2023-11-15 15:02:12 +0300

    Update lst.c

--- lst.c
+++ lst.c
@@ -5,7 +5,6 @@
  * @head: address of pointer to head node
  * @str: str field of node
  * @num: node index used by history
- *
  * Return: size of list
  */
 list_t *add_node(list_t **head, const char *str, int num)
@@ -32,13 +31,11 @@ list_t *add_node(list_t **head, const char *str, int num)
 	*head = new_head;
 	return (new_head);
 }
-
 /**
  * add_node_end - adds a node to the end of the list
  * @head: address of pointer to head node
  * @str: str field of node
  * @num: node index used by history
- *
  * Return: size of list
  */
 list_t *add_node_end(list_t **head, const char *str, int num)
@@ -73,11 +70,9 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 		*head = new_node;
 	return (new_node);
 }
-
 /**
  * print_list_str - prints only the str element of a list_t linked list
  * @h: pointer to first node
- *
  * Return: size of list
  */
 size_t print_list_str(const list_t *h)
@@ -98,7 +93,6 @@ size_t print_list_str(const list_t *h)
  * delete_node_at_index - deletes node at given index
  * @head: address of pointer to first node
  * @index: index of node to delete
- *
  * Return: 1 on success, 0 on failure
  */
 int delete_node_at_index(list_t **head, unsigned int index)
@@ -137,7 +131,6 @@ int delete_node_at_index(list_t **head, unsigned int index)
 /**
  * free_list - frees all nodes of a list
  * @head_ptr: address of pointer to head node
- *
  * Return: void
  */
 void free_list(list_t **head_ptr)
